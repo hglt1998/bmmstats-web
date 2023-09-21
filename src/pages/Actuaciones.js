93ed7f8f6dcef47c90089a4ed3894e4ae60fa27f
Humberto López <g.lops.h1@gmail.com>
2023-09-21 17:02:06 +0200

    Table changes

--- src/pages/Actuaciones.js
+++ src/pages/Actuaciones.js
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import firebase from "../database/firebase";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./actuaciones.css";
 import logo from "../static/LogoSuenaMairenaBlancoIsolated.png";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
 
 export default function Actuaciones() {
   const [events, setEvents] = useState([]);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
+
+  const navigate = useNavigate();
 
   const loadData = () => {
     firebase.db
@@ -29,8 +41,17 @@ export default function Actuaciones() {
           });
         });
         setEvents(events);
-        deleteLoadingAnimation()
+        deleteLoadingAnimation();
       });
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
 
   const deleteLoadingAnimation = () => {
@@ -40,7 +61,7 @@ export default function Actuaciones() {
 
   useEffect(() => {
     loadData();
-  }, []);
+  }, [events]);
 
   return (
     <div className="container">
@@ -63,73 +84,48 @@ export default function Actuaciones() {
           datos
         </p>
       </div>
-      <div className="table-wrapper">
-        <div className="table">
-          <div className="table-header">
-            <div className="table-cell column-1">
-              <p className="header-text">Concepto</p>
-            </div>
-            <div className="table-cell column-2">
-              <p className="header-text">Organizador</p>
-            </div>
-            <div className="table-cell column-3">
-              <p className="header-text">Ciudad</p>
-            </div>
-            <div className="table-cell column-4">
-              <p className="header-text">Fecha</p>
-            </div>
-          </div>
-          <div className="table-wrapper">
-            {events.map((evento) => {
+      <Table className="customTable" sx={{margin: '1vh'}}>
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell>Concepto</TableCell>
+            <TableCell>Organizador</TableCell>
+            <TableCell>Ciudad</TableCell>
+            <TableCell>Fecha</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {events
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((evento, index) => {
               var formatedDate = new Date(evento.fecha.seconds * 1000)
                 .toLocaleString()
                 .toString();
-                formatedDate = formatedDate.substring(0, formatedDate.length - 3)
+              formatedDate = formatedDate.substring(0, formatedDate.length - 3);
               return (
-                <div className="table-row" key={evento.id}>
-                  <div className="table-cell column-1">
-                    {evento.isLive === true ? (
-                      <span className="material-icons blinker">sensors</span>
-                    ) : (
-                      <></>
-                    )}
-                    <Link
-                      className="item-text"
-                      to={`/actuaciones/${evento.id}`}
-                    >
-                      {evento.concepto}
-                    </Link>
-                  </div>
-                  <div className="table-cell column-2">
-                    <Link
-                      className="item-text"
-                      to={`/actuaciones/${evento.id}`}
-                    >
-                      {evento.organizador}
-                    </Link>
-                  </div>
-                  <div className="table-cell column-3">
-                    <Link
-                      className="item-text"
-                      to={`/actuaciones/${evento.id}`}
-                    >
-                      {evento.ciudad}
-                    </Link>
-                  </div>
-                  <div className="table-cell column-4">
-                    <Link
-                      className="item-text"
-                      to={`/actuaciones/${evento.id}`}
-                    >
-                      {formatedDate} h
-                    </Link>
-                  </div>
-                </div>
+                <TableRow
+                  key={evento.id}
+                  onClick={() => navigate(`${evento.id}`)}
+                >
+                  <TableCell align="rigth">{index + 1}</TableCell>
+                  <TableCell align="rigth">{evento.concepto}</TableCell>
+                  <TableCell align="rigth">{evento.organizador}</TableCell>
+                  <TableCell align="rigth">{evento.ciudad}</TableCell>
+                  <TableCell align="rigth">{formatedDate}</TableCell>
+                </TableRow>
               );
             })}
-          </div>
-        </div>
-      </div>
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[25, 50, 100]}
+        component="div"
+        count={events.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </div>
   );
 }
